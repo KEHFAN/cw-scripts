ed73dde359f70421bfbe7aca638efd621898886c
fankehu <fankehu@corp.netease.com>
2024-05-29 18:14:42 +0800

    update cmd

--- install-dependency.cmd
+++ install-dependency.cmd
@@ -1,5 +1,5 @@
 @echo off
-:: 检查window版本
+:: check window os name
 for /f "tokens=2 delims==" %%i in ('wmic os get Caption /value^|find "Caption"') do set var1=%%i
 echo current os name: %var1%
 for /f "tokens=1-3" %%a in ("%var1%") do set "os=%%a %%b %%c"
