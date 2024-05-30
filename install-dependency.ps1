cff690b0bce3518f0dbacc924d5ad06070616de2
fankehu <fankehu@corp.netease.com>
2024-05-30 11:32:07 +0800

    add ps1

---
+++ install-dependency.ps1
@@ -0,0 +1,18 @@
+# 获取当前操作系统名称
+$var1 = (wmic os get Caption /value | Select-String "Caption").ToString() -replace "Caption=",""
+
+# 输出当前操作系统名称
+Write-Host "Current OS name: $var1"
+
+# 根据操作系统名称进行判断
+if ($var1 -like "Microsoft Windows 10 *") {
+    Write-Host "OK"
+    # 可以添加其他命令
+}
+elseif ($var1 -eq "Microsoft Windows 7") {
+    Write-Host "OK 7"
+}
+else {
+    Write-Host "Fail other OS"
+    # 可以添加其他命令
+}
