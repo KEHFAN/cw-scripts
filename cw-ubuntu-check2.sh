9a793ae79ce292af6a5fb17cff2b0c43da80602b
test <test@example.com>
2024-08-16 15:39:40 +0800

    设置apt-get sources.list

--- cw-ubuntu-check2.sh
+++ cw-ubuntu-check2.sh
@@ -3,11 +3,23 @@
 
 
 Get_Pack_Manager(){
+	# 检查apt-get dpkg 文件是否存在
 	if [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
 		PM="apt-get"
 	fi
 }
 
+Set_Repo_Url(){
+	if [ ! -f "/etc/apt/sources.list" ]; then
+		return
+	fi
+	# 修改apt-get镜像源
+	if [ "${PM}"="apt-get" ]; then
+
+		GET_SOURCES_URL=$(cat /etc/apt/sources.list|grep ^deb|head -n 1|awk -F[/:] '{print $4}')
+		echo "${GET_SOURCES_URL}"
+	fi
+}
 
 Install_Main(){
 	Get_Pack_Manager
